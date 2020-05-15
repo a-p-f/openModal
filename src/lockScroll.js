a052489bceed593b35c2daaaf4b9604bed302f7d
Alex Fischer <alex_fischer_81@hotmail.com>
2020-05-15 07:40:32 -0600

    Better scroll lock

--- src/lockScroll.js
+++ src/lockScroll.js
@@ -1,41 +1,34 @@
-/*
-We use 2 different modes of scroll locking, depending on browser configuration.
-
-If the browser uses "only visible while scrolling" scroll bars (newer browsers), then we simply set overflow: hidden on body. This is most efficient.
-
-If the browser has "always visible" scroll bars (which take up space in viewport), then the above technique can cause jarring reflows as scroll bars hide/show. So we use a scroll event listener, instead.
-
-TODO:
-see if we can just set maxHeight/maxWidth on documentElement (read current size), in combination with overflow: hidden. 
-*/
-
-const de = document.documentElement;
-let lockedScrollTop, lockedScrollLeft, overflowX, overflowY;
-function fixScroll() {
-	de.scrollTop = lockedScrollTop;
-	de.scrollLeft = lockedScrollLeft;
-}
-function hasSpaceTakingScrollBars() {
-	return de.clientWidth < window.innerWidth || de.clientHeight < window.innerHeight;
-}
+let width, height, overflowX, overflowY;
 export function lockScroll() {
+	const de = document.documentElement;
+	const ds = de.style;
 	const bs = document.body.style;	
-	lockedScrollTop = de.scrollTop;
-	lockedScrollLeft = de.scrollLeft;
+
+	// Save style values so we can restore
 	overflowX = bs.overflowX;
 	overflowY = bs.overflowY;
+	width = ds.width;
+	height = ds.height;
 
-	if (hasSpaceTakingScrollBars) {
-		addEventListener('scroll', fixScroll);
-	}
-	else {
-		bs.overflowX = 'hidden';
-		bs.overflowY = 'hidden';
-	}
+	/*
+		Lock document dimensions.
+		In some browsers/configurations, toggling overflow will cause scrollbars to hide.
+		Lock dimensions so that the document doesn't reflow.
+	*/
+	ds.width = de.clientWidth + 'px';
+	ds.height = de.clientHeight + 'px';
+
+	// Now lock scrolling
+	bs.overflowX = 'hidden';
+	bs.overflowY = 'hidden';
 }
 export function releaseScroll() {
-	removeEventListener('scroll', fixScroll);
+	const ds = document.documentElement.style;
 	const bs = document.body.style;
-	bs.overflowX = overflowX;
+
 	bs.overflowY = overflowY;
+	bs.overflowX = overflowX;
+
+	ds.width = width;
+	ds.height = height;
 }
